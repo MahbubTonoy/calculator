4965b95baf3859f682113436a74eda6865e071b6
MahbubTonoy <tnymrbadboy@gmail.com>
2021-12-04 20:12:28 +0600

    fix: erase button error fixed

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 let display = document.querySelector(".display span");
-function btnInput(input){
-    display.innerHTML += input;
+let temp;
+function btnInput(input) {
+  display.innerHTML += input;
 }
 
 function erase() {
-    temp = display.innerHTML.substring(0,display.innerHTML.length-1);
-    print(temp);
-   }
-   function reset() {
-       display.innerHTML="";
-   }
-   function action(){
-       let math = eval(display.innerHTML);
-       display.innerHTML = math;
-   }
+  temp = display.innerHTML.substring(0, display.innerHTML.length - 1);
+  display.innerHTML = temp;
+}
+function reset() {
+  display.innerHTML = "";
+}
+function action() {
+  let math = eval(display.innerHTML);
+  display.innerHTML = math;
+}
